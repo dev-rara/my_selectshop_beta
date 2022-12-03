ada7db39efbc383ffb7af48441ef3c8d5d2d9589
rara-dev <kksr1219@gmail.com>
2022-12-03 10:51:05 +0900

    project beta version

---
+++ src/main/java/com/rara/my_selectshop_beta/MySelectshopBetaApplication.java
@@ -0,0 +1,13 @@
+package com.rara.my_selectshop_beta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySelectshopBetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySelectshopBetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rara/my_selectshop_beta/MySelectshopBetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rara.my_selectshop_beta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySelectshopBetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
